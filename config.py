dd9729c892d0d2765f860d4b935c50551057dd2c
Wolfgang Nagele <mail@wnagele.com>
2020-12-15 11:52:39 +0100

    Enable debug port to be used for multiple modes

--- config.py
+++ config.py
@@ -8,14 +8,16 @@ SAMPLE_PERIOD = 30
 RESTART_INTERVAL = 86400
 WATCHDOG_TIMEOUT = SAMPLE_PERIOD + 30  # MUST be higher than SAMPLE_PERIOD + time for transmission
 LEDS = False
-CALIBRATION_MODE = True
 
 #####
 
 import machine
-TEST_MODE = True if machine.Pin("P21", mode = machine.Pin.IN, pull = None).value() == 1 else False
-if TEST_MODE:
+debug_voltage = machine.ADC().channel(pin = "P19", attn = machine.ADC.ATTN_11DB).voltage()
+# Calibration: 220k resistor
+if debug_voltage > 1250 and debug_voltage < 1750:
+    INTERVAL = 60
+    LEDS = False
+# Test: Closed
+elif debug_voltage > 3000:
     INTERVAL = 60
     LEDS = True
-    if CALIBRATION_MODE:
-        LEDS = False
